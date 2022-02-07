0450dfd472ab68458f0041cc7c0636e0d54a2afc
Junqi Wu <wujunqi@tju.edu.cn>
2022-02-07 18:24:16 +0800

    fossil-explorer 1.6 add about.html and relatedwork.html

--- web/script/main.js
+++ web/script/main.js
@@ -3,11 +3,21 @@ let dataList = [
     d3.csv('data/fossil8_20220207.csv', d3.autoType)
 ];
 
-Promise.all(dataList).then(function (datas) {
-    fossilData = datas[1];
-    fossilDataforimg = _.cloneDeep(fossilData);
+window.onload = function () {
+    Promise.all(dataList).then(function (datas) {
+        fossilData = datas[1];
+        fossilDataforimg = _.cloneDeep(fossilData);
 
-    drawTree(datas[0], tree_g, clipTimeWidth, clipTimeHeight)
-    projectMapData(_.cloneDeep(fossilData), node_g, map);
-    mapboxSvg.on('mousemove', event => glyphMousemove(node_g, glyph_g, sunburst_node_g, sunburst_label_g, event, 'map'));
-});
+        // pre-load images
+        fossilData.forEach(function (d) {
+            let img = new Image();
+            img.src = `images/graptolites/${d['fig_name']}`;
+        });
+
+        drawTree(datas[0], tree_g, clipTimeWidth, clipTimeHeight)
+        projectMapData(_.cloneDeep(fossilData), node_g, map);
+        mapboxSvg.on('mousemove', event => glyphMousemove(node_g, glyph_g, sunburst_node_g, sunburst_label_g, event, 'map'));
+    });
+}
+
+
